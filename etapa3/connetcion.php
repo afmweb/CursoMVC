8c989577ad9c4e4599fa65af7706e3bb1ee2f118
AndreFauzeMarcelino <marcelinoandre@hotmail.com.br>
2015-01-21 03:26:48 -0200

    Finalizando a etapa3

--- etapa3/connetcion.php
+++
@@ -1,5 +0,0 @@
-<?php
- $cfg = ActiveRecord\Config::instance();
- $cfg->set_model_directory( DIR_ROOT . '/App/Models');
- $cfg->set_connections(array('development' =>
-  'mysql://USER:PASSWORD@HOST/BANCO'));
